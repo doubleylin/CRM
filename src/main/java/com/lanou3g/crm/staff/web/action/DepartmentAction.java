01e9390416af575a710ff7a4b0f6eeba702b464d
徐颢 <745976932@qq.com>
2017-11-10 21:01:45 +0800

    作者:姚晏霖 项目:CRM登录注册以及登录验证 时间:2017.11.10

---
+++ src/main/java/com/lanou3g/crm/staff/web/action/DepartmentAction.java
@@ -0,0 +1,92 @@
+package com.lanou3g.crm.staff.web.action;
+
+import com.lanou3g.crm.staff.domain.Department;
+import com.lanou3g.crm.staff.service.DepartmentService;
+import com.lanou3g.crm.utils.PageBean;
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * .                       .::::.
+ * .                     .::::::::.
+ * .                    :::::::::::  Jack Slow fuck
+ * .                ..:::::::::::'
+ * .            '::::::::::::::'
+ * .               ':::::::::::
+ * .           '::::::::::::::..
+ * .                ..::::::::::::.
+ * .              ``::::::::::::::::
+ * .               ::::``:::::::::'        .:::.
+ * .              ::::'   ':::::'       .::::::::.
+ * .            .::::'      ::::     .:::::::'::::.
+ * .           .:::'       :::::  .:::::::::' ':::::.
+ * .          .::'        :::::.:::::::::'      ':::::.
+ * .         .::'         ::::::::::::::'         ``::::.
+ * .     ...:::           ::::::::::::'              ``::.
+ * .    ```` ':           ':::::::::'                  ::::..
+ * .                       '.:::::'                    ':'````..
+ */
+@Controller("departmentAction")
+@Scope("prototype")
+public class DepartmentAction extends ActionSupport implements ModelDriven<Department> {
+    private Department department = new Department();
+
+    private DepartmentService departmentService;
+
+    private int pageNum;
+    private int pageSize = 5;
+
+    public String addDepartment(){
+        if (department.getDepName().trim().equals("")){
+            addActionError("部门名称空了!");
+            return INPUT;
+        }
+        if (department.getDepId().equals("")){
+            departmentService.addDepartment(department);
+        }else {
+            departmentService.updateDepattment(department);
+        }
+        return SUCCESS;
+    }
+    public String findAllDeptByPage(){
+        if (pageNum==0){
+            pageNum=1;
+        }
+        PageBean<Department> data = departmentService.findAllDepartmentP(department,pageNum,pageSize);
+        ActionContext.getContext().put("pageBean",data);
+        return SUCCESS;
+    }
+
+    public String findAllDept(){
+        List<Department> departments = departmentService.findAllDepartment();
+        ActionContext.getContext().getSession().put("departments",departments);
+        return SUCCESS;
+    }
+    @Override
+    public Department getModel() {
+        return null;
+    }
+    
+
+    public int getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(int pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+}
